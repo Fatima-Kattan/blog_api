61469df76a818e944be97784abf7775ccd7d87c9
RayaNaSSi <rassi7266@gmail.com>
2026-01-12 19:04:07 +0300

    add not-followings route

--- app/Http/Controllers/Api/FollowController.php
+++ app/Http/Controllers/Api/FollowController.php
@@ -5,25 +5,26 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Follow;
 use App\Models\Notification;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class FollowController extends Controller
 {
     public function index()
-{
-    $userId = Auth::id();
+    {
+        $userId = Auth::id();
 
-    $followers = Follow::where('following_id', $userId)->with('follower')->get();
-    $followings = Follow::where('follower_id', $userId)->with('following')->get();
+        $followers = Follow::where('following_id', $userId)->with('follower')->get();
+        $followings = Follow::where('follower_id', $userId)->with('following')->get();
 
-    return response()->json([
-        'followers_count' => $followers->count(),
-        'followers' => $followers,
-        'followings_count' => $followings->count(),
-        'followings' => $followings,
-    ]);
-}
+        return response()->json([
+            'followers_count' => $followers->count(),
+            'followers' => $followers,
+            'followings_count' => $followings->count(),
+            'followings' => $followings,
+        ]);
+    }
 
     // عمل متابعة جديدة
     public function store(Request $request)
@@ -53,7 +54,7 @@ class FollowController extends Controller
             'comment_id' => null,
             'is_read'    => false,
         ]);
-    
+
         return response()->json($follow, 201);
     }
 
@@ -101,4 +102,23 @@ class FollowController extends Controller
         ]);
     }
 
+    //عرض الاشخاص اللي لا اتابعهم
+    public function notFollowings($userId)
+    {
+        // IDs الأشخاص اللي المستخدم متابعن
+        $followingsIds = Follow::where('follower_id', $userId)
+            ->pluck('following_id');
+
+        // كل المستخدمين ما عدا اللي متابعن
+        $notFollowings = User::whereNotIn('id', $followingsIds)
+            ->where('id', '!=', $userId) // استثناء نفسه
+            ->get();
+
+        $count = $notFollowings->count();
+
+        return response()->json([
+            'count' => $count,
+            'data' => $notFollowings
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -68,6 +68,9 @@ Route::prefix('v1')->group(function () {
         Route::get('follows', [FollowController::class, 'index']);
         Route::get('users/{id}/followers', [FollowController::class, 'followers']);
         Route::get('users/{id}/followings', [FollowController::class, 'followings']);
+        Route::get('/users/{id}/not-followings', [FollowController::class, 'notFollowings']);
+
+
         
         //الاشعارات
         Route::get('/notifications', [NotificationController::class, 'index']); 
